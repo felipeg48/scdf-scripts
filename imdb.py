eca6ca08b247256c1d8b0d89485a2551a8c1f907
Felipe Gutierrez <felipeg@gmail.com>
2020-05-13 23:35:34 -0600

    Update imdb.py

--- imdb.py
+++ imdb.py
@@ -1,16 +1,26 @@
 import json
 
+cheat = {}
+
 def input():
   print ("PRE: " + payload)
   print ("Type: ")
   print (type(payload))
   print (payload[0])
-  print (headers)
+  cheat = payload
+  
+  for i in payload:
+    print (i)
   
   return {'url':'https://imdb8.p.rapidapi.com/title/get-details?tconst=tt0944947'}
 
 def output():
-  print ("POST" + payload)
+  print ("POST: " + payload)
+  print ("Cheat: ")
+  print (type(payload))
+  print (cheat)
+  print ("-------")
+  
   return json.loads(payload)
 
 
