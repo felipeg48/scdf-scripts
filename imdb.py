1835ff31a07565af7f5366e1d8c1f228de36aab1
Felipe Gutierrez <felipeg@gmail.com>
2020-05-13 23:30:41 -0600

    Update imdb.py

--- imdb.py
+++ imdb.py
@@ -4,6 +4,8 @@ def input():
   print ("PRE: " + payload)
   print ("Type: ")
   print (type(payload))
+  print (payload[0])
+  print (headers)
   
   return {'url':'https://imdb8.p.rapidapi.com/title/get-details?tconst=tt0944947'}
 
