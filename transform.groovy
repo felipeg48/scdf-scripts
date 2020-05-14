7ca24e9c9519ae526e0d3b3166b3a6d4d49d8502
Felipe Gutierrez <felipeg@gmail.com>
2020-05-14 12:12:03 -0600

    Update transform.groovy

--- transform.groovy
+++ transform.groovy
@@ -1,9 +1,29 @@
 import groovy.json.JsonSlurper
 import groovy.json.JsonOutput
 
+
 def jsonSlurper = new JsonSlurper()
-def json = jsonSlurper.parseText(new String(payload))
+def movie = jsonSlurper.parseText(new String(payload))
 
-json.msg = json.msg.toUpperCase()
 
-JsonOutput.toJson(json)
+def connection = new URL( "https://imdb8.p.rapidapi.com/title/get-details?tconst=${movie.id}")
+                 .openConnection() as HttpURLConnection
+
+connection.setRequestProperty( 'x-rapidapi-host', 'imdb8.p.rapidapi.com' )
+connection.setRequestProperty( 'x-rapidapi-key', '2ee82876a6msh3fc242639f601a0p13b445jsncf1ac4d987c0')
+connection.setRequestProperty( 'Accept', 'application/json' )
+connection.setRequestProperty( 'Content-Type', 'application/json')
+
+if ( connection.responseCode == 200 ) {
+    
+    def imdb = connection.inputStream.withCloseable { inStream ->
+        new JsonSlurper().parse( inStream as InputStream )
+    }
+    
+    movie.imdb = [ "image": imdb.image.url ]   
+
+} else {
+    println connection.responseCode + ": " + connection.inputStream.text
+}
+
+JsonOutput.toJson(movie)
